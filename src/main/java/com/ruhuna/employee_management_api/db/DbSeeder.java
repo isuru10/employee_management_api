a4e16f5e76ba7c857a0c63700b2b03fdeebe0fc7
isuru10 <isurutennakoon95@gmail.com>
2019-02-08 14:35:40 +0530

    Data Seeder Removed

--- src/main/java/com/ruhuna/employee_management_api/db/DbSeeder.java
+++ src/main/java/com/ruhuna/employee_management_api/db/DbSeeder.java
@@ -21,36 +21,36 @@ public class DbSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        this.employeeRepository.deleteAll();
-        this.skillRepository.deleteAll();
-
-        Skill skill1 = new Skill("Java");
-        Skill skill2 = new Skill("Angular");
-        Skill skill3 = new Skill("Postgres");
-
-        Employee employee = new Employee("Isuru", "isuru@gmail.com", new SimpleDateFormat("dd/MM/yyyy").parse("18/08/1995"));
-        Employee employee2 = new Employee("Bathiya", "bathiya@gmail.com", new SimpleDateFormat("dd/MM/yyyy").parse("18/01/1994"));
-
-        employee.getSkills().add(skill1);
-        employee.getSkills().add(skill2);
-        employee.getSkills().add(skill3);
-
-        employee2.getSkills().add(skill1);
-        employee2.getSkills().add(skill2);
-        employee2.getSkills().add(skill3);
-
-        skill1.getEmployees().add(employee);
-        skill2.getEmployees().add(employee);
-        skill3.getEmployees().add(employee);
-
-        skill1.getEmployees().add(employee2);
-        skill2.getEmployees().add(employee2);
-        skill3.getEmployees().add(employee2);
-
-        employeeRepository.save(employee);
-        employeeRepository.save(employee2);
-
-        employee2.setName("Lochana");
-        employeeRepository.save(employee2);
+//        this.employeeRepository.deleteAll();
+//        this.skillRepository.deleteAll();
+//
+//        Skill skill1 = new Skill("Java");
+//        Skill skill2 = new Skill("Angular");
+//        Skill skill3 = new Skill("Postgres");
+//
+//        Employee employee = new Employee("Isuru", "isuru@gmail.com", new SimpleDateFormat("dd/MM/yyyy").parse("18/08/1995"));
+//        Employee employee2 = new Employee("Bathiya", "bathiya@gmail.com", new SimpleDateFormat("dd/MM/yyyy").parse("18/01/1994"));
+//
+//        employee.getSkills().add(skill1);
+//        employee.getSkills().add(skill2);
+//        employee.getSkills().add(skill3);
+//
+//        employee2.getSkills().add(skill1);
+//        employee2.getSkills().add(skill2);
+//        employee2.getSkills().add(skill3);
+//
+//        skill1.getEmployees().add(employee);
+//        skill2.getEmployees().add(employee);
+//        skill3.getEmployees().add(employee);
+//
+//        skill1.getEmployees().add(employee2);
+//        skill2.getEmployees().add(employee2);
+//        skill3.getEmployees().add(employee2);
+//
+//        employeeRepository.save(employee);
+//        employeeRepository.save(employee2);
+//
+//        employee2.setName("Lochana");
+//        employeeRepository.save(employee2);
     }
 }
